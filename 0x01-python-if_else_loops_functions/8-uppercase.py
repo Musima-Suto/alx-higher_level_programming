e5631ff64b61de9208c39b13177b9e0aa363efc7
Musima-Suto <sutomusima@gmail.com>
2022-06-27 14:05:46 +0000

    Fix pycodestyle error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     for i in str:
         if ord(i) in range(97, 123):
             upper = ord(i) - 32
-	    new_str = new_str + chr(upper)
+            new_str = new_str + chr(upper)
         else:
             new_str = new_str + i
     print("{}".format(new_str))
