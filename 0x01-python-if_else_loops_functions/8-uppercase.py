da78f988a1fa091df9bfb476e958fb53a1056695
Musima-Suto <sutomusima@gmail.com>
2022-06-27 12:23:08 +0000

    Print in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = " "
+    for i in str:
+        if ord(i) in range(97, 123):
+            upper = ord(i) - 32
+	    new_str = new_str + chr(upper)
+        else:
+            new_str = new_str + i
+    print(new_str)
