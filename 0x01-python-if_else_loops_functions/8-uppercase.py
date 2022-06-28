94fd902abb5c5dda72ff18e6d8043801361733fc
Musima-Suto <sutomusima@gmail.com>
2022-06-28 16:49:18 +0000

    Fix checker error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    new_str = " "
     for i in str:
         if ord(i) in range(97, 123):
             upper = ord(i) - 32
-            new_str = new_str + chr(upper)
-        else:
-            new_str = new_str + i
-    print("{}".format(new_str))
+            str = str + chr(upper)
+    print("{}".format(str))
