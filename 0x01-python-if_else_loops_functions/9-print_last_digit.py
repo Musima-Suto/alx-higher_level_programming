c5a8224ec6cc9d11e87b6bafc7bbd4075a60d0c0
Musima-Suto <sutomusima@gmail.com>
2022-06-29 18:13:01 +0000

    Fix checker error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,5 @@
 def print_last_digit(number):
     if number < 0:
         number = number * -1
-        last_digit = number % 10
-    else:
-        last_digit = number % 10
+    last_digit = number % 10
     print(last_digit)
