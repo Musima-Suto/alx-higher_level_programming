775d67b0f918834cfbaa490f3384a98491265123
Musima-Suto <sutomusima@gmail.com>
2022-07-04 19:31:02 +0000

    Count number of arguements

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    number = len(argv) - 1
+    if number == 1:
+        print("{} argument:".format(1))
+    elif number == 0:
+        print("{} arguments.".format(0))
+    elif number > 1:
+        print("{} arguements:".format(number))
+    for i in range(number):
+        print("{}:{}".format(i+1, sys.argv[i+1]))
