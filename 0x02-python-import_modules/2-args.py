a08b98d43273a5c2d41356781229696bc946ff30
Musima-Suto <sutomusima@gmail.com>
2022-07-04 20:33:05 +0000

    Fix typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    number = len(ys.rgv) - 1
+    number = len(sys.argv) - 1
     if number == 1:
         print("{} argument:".format(1))
     elif number == 0:
